cf00e5c7f121939b26f7054a6faca2aeb5cce972
JeekLee <j3kkun@gmail.com>
2022-12-12 17:54:25 +0900

    [Add] Skeleton Code

---
+++ src/main/java/com/example/projectbluehair/ProjectBlueHairApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectbluehair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlueHairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBlueHairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectbluehair/ProjectBlueHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectbluehair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlueHairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
