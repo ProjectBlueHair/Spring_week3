57e9a7a9ea1627f701309704ef8bb544b4d0e494
JeekLee <j3kkun@gmail.com>
2022-12-13 23:51:28 +0900

    feat: JWT
    JWT Exception Handling 오류 처리
    
    - 기존 방식은 Controller 계층에서 에러 처리를 하는
    방식이었음.
    - JWT 관련 Exception은 Filter에서 발생하기 때문에, Controller 계층에서 처리가
    불가능함.
    - 해결은 다음과 같음.
    
    1. AccessDeniedHandler, AuthenticationEntryPoint를
    상속받은 클래스 선언
    2. @Override를 통해 Method들을 재정의하고, ResponseDto 객체에 담아 반환하도록
    함.
     - SuccessResponse를 사용하려 했으나, Initialize가 되어있지 않다는 에러와 함께 컴파일이 불가능 했음. DI/IoC는 정상적으로 설정되어 있었음. (Component, RequiredArgsConstructor 등)
     - 원인은 확인 필요

---
+++ src/main/java/com/example/projectbluehair/common/security/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.example.projectbluehair.common.security.exception;
+
+import com.example.projectbluehair.common.response.ResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    ResponseDto responseDto = new ResponseDto(HttpStatus.UNAUTHORIZED, "토큰 권한 오류", null);
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authenticationException) throws IOException {
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        try (OutputStream os = response.getOutputStream()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(os, responseDto);
+            os.flush();
+        }
+    }
+}
